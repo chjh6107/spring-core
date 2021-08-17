89710eabb073bb6e0b1d3eff92a551f958272786
chjh6107 <chjh6107@gmail.com>
2021-08-17 15:37:21 +0900

    도메인 개발

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,20 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//아직 DB가 정해지지 않았다는 전제하에 대충 하나 만든것
+public class MemoryMemberRepository implements MemberRepository{
+    private static Map<Long, Member> store=new HashMap<>();
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(),member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+    
+}
