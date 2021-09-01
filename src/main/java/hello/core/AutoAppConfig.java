27b18929710e5b1817c851b35c3c8bd31afe6cd2
chjh6107 <chjh6107@gmail.com>
2021-09-01 14:31:10 +0900

    필터테스트

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -8,6 +8,9 @@ import org.springframework.context.annotation.FilterType;
  //자동으로 스프링 빈으로 올려주는 애노테이션, 
 //Component 애노테이션이 붙은 클래스를 찾아 자동으로 빈에 등록
 @ComponentScan(
+
+    //basePackges를 사용하면 입력한 디렉터리 하위만 스캔의 대상이 됨
+    basePackages="hello.core.member",
     //등록할때 뺄것 지정
     //AppConfig에 Configuration이 있고, 수동으로 Bean에다가 올려주는 설정을 하는데
     //자동으로 등록되면 안되니 Configuration.class를 등록안하게 함
