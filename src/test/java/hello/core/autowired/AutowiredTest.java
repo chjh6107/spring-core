77c463e468d3599b5dafc5c314bf007fc84fb87e
chjh6107 <chjh6107@gmail.com>
2021-09-03 20:32:52 +0900

    옵션처리 주석

--- src/test/java/hello/core/autowired/AutowiredTest.java
+++ src/test/java/hello/core/autowired/AutowiredTest.java
@@ -17,17 +17,17 @@ public class AutowiredTest {
 
     }
     static class TestBean{
-        @Autowired(required = false)
+        @Autowired(required = false) //자동 주입할 대상이 없으면 수정자 메서드 자체가 호출 안됨
         public void setNoBean1(Member noBean1){
             System.out.println("noBean1 = "+noBean1);
         }
 
-        @Autowired
+        @Autowired //nullable은 자동 주입할 대상이 없으면 null이 입력됨
         public void setNoBean2(@Nullable Member noBean2){
             System.out.println("noBean2 = "+noBean2);
         }
 
-        @Autowired
+        @Autowired //자동 주입할 대상이 없으면 Optional.empty가 입력됨
         public void setNoBean3(Optional<Member> noBean3){
             System.out.println("noBean3 = "+noBean3);
         }
