1884eb33bd2295c662a392689b3220a309097a2d
chjh6107 <chjh6107@gmail.com>
2021-09-08 22:12:41 +0900

    provider

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -4,11 +4,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
+
+import hello.core.scope.PrototypeTest.PrototypeBean;
 
 public class SingletonWithPrototypeTest1 {
     
@@ -34,21 +38,29 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2=clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     
     }
 
+    //Provider 방법은 자바 표준이긴 하나, gradle에 라이브버리를 입력해주어야한다
+    // implementation 'javax.inject:javax.inject:1'
     @Scope("singleton")
     static class ClientBean{
-        private final PrototypeBean prototypeBean; //생성시점에 주입되어버려 계속 같은 것을 사용ㄹ
+        // private final PrototypeBean prototypeBean; //생성시점에 주입되어버려 계속 같은 것을 사용ㄹ
+
+        @Autowired
+        // private ObjectProvider<PrototypeBean> prototypeBeanProvider; //DL방법
+        private Provider<PrototypeBean> prototypeBeanProvider;//Provider 방법
 
         //생성자 한개라 Autowired 생략가능
-        @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean=prototypeBean;
-        }
+        // @Autowired
+        // public ClientBean(PrototypeBean prototypeBean) {
+        //     this.prototypeBean=prototypeBean;
+        // }
 
         public int logic(){
+            // PrototypeBean prototypeBean = prototypeBeanProvider.getObject(); //DL방법
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             int count=prototypeBean.getCount();
             return count;
