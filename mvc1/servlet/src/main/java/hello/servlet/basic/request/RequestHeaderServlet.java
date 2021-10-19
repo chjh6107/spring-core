254249b91e472389be44e08197f6fc8945edd924
chjh6107 <chjh6107@gmail.com>
2021-10-19 23:02:33 +0900

    파라미터 받기

--- mvc1/servlet/src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
+++ mvc1/servlet/src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -1,7 +1,6 @@
 package hello.servlet.basic.request;
 
 import java.io.IOException;
-import java.util.Enumeration;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
